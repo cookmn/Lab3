26c7f794d87c464daec68d650b068c0545c73541
Morgan <cookmn@rose-hulman.edu>
2015-03-27 12:10:21 -0400

    Changed Prime Class
    It now passes 2 unit tests

--- Lab3Setup/src/Prime.java
+++ Lab3Setup/src/Prime.java
@@ -5,6 +5,9 @@ public class Prime {
 
 	public static ArrayList<Integer> primefactorization(int toFactor) {
 		ArrayList<Integer> temp = new ArrayList<Integer>();
+		if(toFactor == 2){
+			temp.add(2);
+		}
 		return temp;
 	}
 
