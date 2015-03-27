ca0822c59018fbf27efdb114062957c85f6539e5
Morgan <cookmn@rose-hulman.edu>
2015-03-27 12:09:04 -0400

    Added Second Unit Test
    Which fails

--- Lab3Setup/src/Lab3Test.java
+++ Lab3Setup/src/Lab3Test.java
@@ -12,5 +12,13 @@ public class Lab3Test {
 		ArrayList<Integer> value = new ArrayList<Integer>();
 		assertEquals(Prime.primefactorization(1), value); 
 	}
+	
+	@Test
+	public void testFor2() {
+		ArrayList<Integer> value = new ArrayList<Integer>();
+		value.add(2);
+		System.out.println(value);
+		assertEquals(Prime.primefactorization(2), value);
+	}
 
 }
