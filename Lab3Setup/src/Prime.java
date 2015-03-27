e2bda95669fbd86a2f35e97a756ad4b677628e1d
Morgan <cookmn@rose-hulman.edu>
2015-03-27 13:12:44 -0400

    Modified the Prime Class
    It now passes the 4th unit test and is more generic

--- Lab3Setup/src/Prime.java
+++ Lab3Setup/src/Prime.java
@@ -5,11 +5,13 @@ public class Prime {
 
 	public static ArrayList<Integer> primefactorization(int toFactor) {
 		ArrayList<Integer> temp = new ArrayList<Integer>();
-		if(toFactor == 2){
+		while (toFactor%2 == 0){
 			temp.add(2);
-		} else if(toFactor == 3){
+			toFactor = toFactor / 2;
+		} while(toFactor % 3 == 0){
 			temp.add(3);
-		}
+			toFactor = toFactor / 3;
+		} 
 		return temp;
 	}
 
